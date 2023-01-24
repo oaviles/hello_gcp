5871c1bf3c847eef9ae7678658ce61c8f2f0c3e8
Omar Aviles <oaviles@live.com>
2023-01-24 08:46:55 -0600

    Create variables.tf

---
+++ IaC/variables.tf
@@ -0,0 +1,43 @@
+variable "gcp_project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "gcpcredentials" {
+  description = "GCP Project credentials filename"
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "GCP region where resource will be created"
+  type        = string
+  #default     = "us-central1" #us-west1
+}
+
+variable "gke_cluster_name" {
+  description = "GKE cluster name"
+  type        = string
+  default     = "gkecluster-demo"
+}
+
+variable "admin_username" {
+  description = "GKE control plane administrator username"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "GKE control plane administrator username"
+  type        = string
+}
+
+variable "gke_cluster_node_count" {
+  description = "GKE cluster node count"
+  type        = number
+  default     = 1
+}
+
+variable "gke_cluster_node_machine_type" {
+  description = "GKE cluster node machine type"
+  type        = string
+  default     = "n1-standard-2"
+}
