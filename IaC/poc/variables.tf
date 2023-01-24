6604dcf4c354088a832ef1ee42e78565e202aeb1
Omar Aviles <oaviles@live.com>
2023-01-24 11:57:11 -0600

    Create variables.tf

---
+++ IaC/poc/variables.tf
@@ -0,0 +1,15 @@
+variable "gcp_project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "gcpcredentials" {
+  description = "GCP Project credentials filename"
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "GCP region where resource will be created"
+  type        = string
+  default     = "us-central1" #us-west1
+}
